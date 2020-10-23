48c87f986de8ba00a83a3af6290c6f16b3f623e2
dawidl022 <68463406+dawidl022@users.noreply.github.com>
2020-10-23 16:07:19 +0100

    Wprowadź klucze API tutaj

--- config_template.py
+++ config_template.py
@@ -1,3 +1,5 @@
+# po wpisaniu kluczy, zmień nazwy pliku na config.py
+
 api_headers = {"app_id": "EDAMAN_API_ID", "app_key": "EDAMAN_API_KEY"}
 ibm_api = {"key": "WATSON_API_ID",
            "url": "WATSON_URL"}
